820b84305605e70d6302b57578b75558a9ac4e95
marlena91 <marlena.zarczynska@gmail.com>
2022-05-06 22:41:32 -0700

    First exercise, part2

--- src/Animal.java
+++ src/Animal.java
@@ -1,7 +1,49 @@
 public class Animal {
-
+    private static final Double DEFAULT_DOG_WEIGHT = 9.0;
+    static final Double DEFAULT_WEIGHT = 5.0;
+    static final Double DEFAULT_CAT_WEIGHT = 2.0;
     String name;
     Integer age;
-    Double weight;
     Boolean isAlive;
+    final String species;
+    private Double weight;
+
+
+    public Animal(String species) {
+        this.isAlive = true;
+        this.age = 1;
+        this.species = species;
+
+        switch (this.species) {
+            case "cat" -> this.weight = DEFAULT_CAT_WEIGHT;
+            case "dog" -> this.weight = DEFAULT_DOG_WEIGHT;
+            default -> this.weight = DEFAULT_WEIGHT;
+        }
+    }
+
+    public Double getWeight() {
+        return this.weight;
+    }
+
+    void feed() {
+        if (!isAlive) {
+            System.out.println("You cannot feed a dead dog!");
+
+        } else {
+            weight += 1;
+            System.out.println(name + " said: Thx for food!");
+        }
+
+    }
+
+    void takeForAWalk() {
+        if (weight == 0) {
+            isAlive = false;
+            System.out.println("This dog is dead!");
+
+        } else {
+            weight -= 1;
+            System.out.println(name + " said: nice walk, thx!");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Animal cat = new Animal();
+        Animal cat = new Animal("cat");
         cat.name = "Luna";
-        cat.weight = 3.2;
 
-        System.out.println(cat.name);
-        System.out.println("Waga kota: " + cat.weight + "kg");
+        System.out.println();
+        System.out.println(cat.name + " weight: " + cat.getWeight() + "kg.");
+
+
+        Animal dog = new Animal("dog");
+        dog.name = "Azor";
+
+        System.out.println(dog.name + " weight: " + dog.getWeight() + "kg.");
+
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.takeForAWalk();
+        dog.feed();
+
 
     }
 }
